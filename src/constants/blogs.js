cd76af8eb5a0f284b696405a53ec8280460f037a
sc <dsc@vaults.exchange>
2024-04-12 17:19:57 +0900

    blog test

---
+++ src/constants/blogs.js
@@ -0,0 +1,36 @@
+// <img> tag example (change "1b" to target) : <br/><div style="display: flex;justify-content: center;"}><img src="/blogs/1b.png" alt="1b" /></div>
+
+export const blogs = [
+  {
+    id: 1,
+    title: "偏財運強的命盤特徵",
+    contentHtmls: [
+      "在星軌理數中，主要的偏財星有三顆，分別是「破軍」、「貪狼」、「廉貞」。如果有果報的宮位，化偏財星的祿，會有偏財運好的跡象。",
+      "果報宮分別有福德宮、子女宮、夫妻宮、遷移宮。其中福德宮和遷移宮的力量最強。",
+      "所以，如果你有以下特徵，就代表偏財運強，容易中獎：",
+      '1. 生年偏財星祿 (破 / 廉 / 貪) 坐福德宮<br/><div style="display: flex;justify-content: center;"}><img src="/blogs/1a.png" alt="1a" /></div>',
+      '2. 福德宮化偏財星祿 (破 / 廉 / 貪)<br/><div style="display: flex;justify-content: center;"}><img src="/blogs/1b.png" alt="1b" /></div>',
+      '3. 遷移宮化偏財星祿 (破 / 廉 / 貪)<br/><div style="display: flex;justify-content: center;"}><img src="/blogs/1c.png" alt="1c" /></div>',
+      '大家可以排盤看看，自己有沒有以上結構。<a href="/chart" target="_blank">排盤連結</a>',
+      "但即使偏財運強，也建議理性投資 / 賭博，小賭怡情！",
+    ],
+  },
+  {
+    id: 2,
+    title: "偏財運強的命盤特徵",
+    contentHtmls: [
+      "在星軌理數中，主要的偏財星有三顆，分別是「破軍」、「貪狼」、「廉貞」。如果有果報的宮位，化偏財星的祿，會有偏財運好的跡象。",
+      "果報宮分別有福德宮、子女宮、夫妻宮、遷移宮。其中福德宮和遷移宮的力量最強。",
+      "所以，如果你有以下特徵，就代表偏財運強，容易中獎：",
+      '1. 生年偏財星祿 (破 / 廉 / 貪) 坐福德宮<br/><div style="display: flex;justify-content: center;"}><img src="/blogs/1a.png" alt="1a" /></div>',
+      '2. 福德宮化偏財星祿 (破 / 廉 / 貪)<br/><div style="display: flex;justify-content: center;"}><img src="/blogs/1b.png" alt="1b" /></div>',
+      '3. 遷移宮化偏財星祿 (破 / 廉 / 貪)<br/><div style="display: flex;justify-content: center;"}><img src="/blogs/1c.png" alt="1c" /></div>',
+      '大家可以排盤看看，自己有沒有以上結構。<a href="/chart" target="_blank">排盤連結</a>',
+      "但即使偏財運強，也建議理性投資 / 賭博，小賭怡情！",
+    ],
+  },
+  {
+    id: 3,
+    title: "test",
+  },
+];
---
+++ src/pages/blog/index.js
@@ -0,0 +1,104 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import aboutStyle from "@/styles/About.module.scss";
+import blogStyle from "@/styles/Blog.module.scss";
+import { Divider } from "@mui/material";
+
+import { blogs } from "@/constants/blogs";
+
+const BlogContent = ({ blog }) => {
+  return (
+    <>
+      <div className={`${blogStyle.container}`} id={`blog${blog.id}`}>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className={aboutStyle.pageTitle}>{blog.title}</div>
+        <div className={aboutStyle.pageText}>
+          {blog.contentHtmls && blog.contentHtmls.length > 0
+            ? blog.contentHtmls.map((contentHtml, cIndex) => (
+                <div key={`${blog.title}content${cIndex}`}>
+                  <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+                  <br />
+                </div>
+              ))
+            : null}
+        </div>
+        <br />
+        <br />
+        <Divider light variant="middle" sx={{ margin: "20px 0" }} />
+      </div>
+    </>
+  );
+};
+
+export default function Blog() {
+  return (
+    <>
+      <Head>
+        <title>曜靈星軌理數 - 您的智能人生定位系統</title>
+      </Head>
+      <div className={`header show`}>
+        <div className="left info-header">
+          <Link href="/">
+            <div className="logo">
+              <img src={"logo.png"} alt="logo" />
+              <div className="name">曜靈星軌理數</div>
+            </div>
+          </Link>
+          <Link href="/chart">
+            <button>排盤</button>
+          </Link>
+          <Link href="#begin">
+            <button>遇見命理師</button>
+          </Link>
+          <Link href="#analysis">
+            <button>命理分析</button>
+          </Link>
+          <Link href="#course">
+            <button>教學課程</button>
+          </Link>
+          <Link href="#question">
+            <button>常見問題</button>
+          </Link>
+          <Link href="#contact">
+            <button>立即預約</button>
+          </Link>
+        </div>
+        <div className="right">
+          <a target="_blank" href="https://buy.stripe.com/cN2cPsa6XaLMauQ001">
+            支持我們
+          </a>
+        </div>
+      </div>
+
+      <div className={`${blogStyle.split} ${blogStyle.left}`}>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className={`${blogStyle.content}`}>
+          {blogs.map((blog, bIndex) => (
+            <div key={`blogContent${bIndex}`}>
+              <a href={`#blog${blog.id}`}>{`${blog.id}. ${blog.title}`}</a>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={`${blogStyle.split} ${blogStyle.right}`}>
+        {blogs.map((blog, bIndex) => (
+          <BlogContent blog={blog} key={`blog${bIndex}`} />
+        ))}
+
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
+}
