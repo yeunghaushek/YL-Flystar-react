2bd99315616803162f24ded77ab83aae03a6ed52
sc <dsc@vaults.exchange>
2024-08-13 13:40:31 +0900

    link update

--- src/pages/blog/[blogCate]/[blogId].js
+++ src/pages/blog/[blogCate]/[blogId].js
@@ -98,7 +98,13 @@ const BlogCate = () => {
           name="description"
           content="發掘您的人生地圖！我們提供專業命理分析，助您預見未來趨勢與機遇，規劃事業與人生策略。立即探索，打造成功的人生藍圖。"
         />
-        <meta name="description" content={`${myBlog?.blogTitle ?? ``}`} />
+        <meta
+          name="description"
+          content={`${
+            myBlog?.blogTitle ??
+            `發掘您的人生地圖！我們提供專業命理分析，助您預見未來趨勢與機遇，規劃事業與人生策略。立即探索，打造成功的人生藍圖。`
+          }`}
+        />
         <link rel="canonical" href={`https://yl-flystar.pro/blog/${blogCate}/${blogId}`} />
       </Head>
       <Header />
